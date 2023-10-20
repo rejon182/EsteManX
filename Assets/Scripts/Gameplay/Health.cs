bdb375da056af35c77a5de97fa4d60596f9e9666
Eduardo Rejon <66136404+rejon182@users.noreply.github.com>
2023-10-20 17:39:23 -0600

    Movimiento base
    Es el movimiento visto en clase. Se incluyue doble salto, salto en pared y deslizamiento en ella

---
+++ Assets/Scripts/Gameplay/Health.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+	
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);			
+        }
+    }
+	
+    public void TakeHealth(int vida)
+    {
+        health += vida;
+        if (health >= 100)
+        {
+            health = 100;
+        }
+    }
+}
