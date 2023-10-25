7d8afff560d7882cd63e2fac1c93e0a38dfbea73
Eduardo Rejon <66136404+rejon182@users.noreply.github.com>
2023-10-24 19:38:44 -0600

    Enemigo que dispara

--- Assets/Scripts/Gameplay/PitcherEnemyActivate.cs
+++ Assets/Scripts/Gameplay/PitcherEnemyActivate.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PitcherEnemyActive : MonoBehaviour
+public class PitcherEnemyActivate : MonoBehaviour
 {
     public GameObject bulletPref;
     public float aimingTime = 0.5f;
     public float shootingTime = 1.5f;
-	
+
     public Transform firePoint;
     bool isAttack;
     Animator anim;
-	
+
     void Awake()
     {
         anim = GetComponentInParent<Animator>();
@@ -19,11 +19,15 @@ public class PitcherEnemyActive : MonoBehaviour
 
     void Shoot()
     {
+        Debug.Log("Funcion shoot");
+        Debug.Log("bulletPref: " + bulletPref );
+        Debug.Log("firePoint: " + firePoint );
         if (bulletPref != null && firePoint != null)
         {
+            Debug.Log("Shoot! isAttack: "+ isAttack);
             GameObject bulletP = Instantiate(bulletPref, firePoint.position, Quaternion.identity) as GameObject;
             Bullet bulletScript = bulletP.GetComponent<Bullet>();
-			
+
             if (transform.localScale.x < 0)
             {
                 bulletScript.direccion = Vector2.right;
@@ -34,19 +38,22 @@ public class PitcherEnemyActive : MonoBehaviour
             }
         }
     }
-	
+
     void OnTriggerStay2D(Collider2D other)
     {
         if (isAttack == false && other.CompareTag("Player"))
         {
+            Debug.Log("Into Compare Tag");
             StartCoroutine(ShootTime());
         }
     }
+
     IEnumerator ShootTime()
     {
         isAttack = true;
         yield return new WaitForSeconds(aimingTime);
-        anim.SetTrigger("isShoot");
+        // anim.SetTrigger("isShoot");
+        Shoot();
         yield return new WaitForSeconds(shootingTime);
         isAttack = false;
     }
